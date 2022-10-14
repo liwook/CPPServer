cc2437ce4ebeafe84557c57d90757416f9446ad1
lihaijie <lhjwook123456@163.com>
2022-10-14 20:14:18 +0800

    函数添加const

---
+++ code/server_v4/InetAddr/InetAddr.h
@@ -0,0 +1,19 @@
+#pragma once
+#include<string>
+#include<arpa/inet.h>
+#include<stdio.h>
+class InetAddr
+{
+public:
+	InetAddr();
+	InetAddr(unsigned short port, const char* ip=nullptr);
+	const sockaddr_in* getAddr()const { return &addr_; }
+	void setAddr(const struct sockaddr_in& addr) { addr_ = addr; }
+
+	std::string toIp()const;
+	std::string toIpPort()const;
+	unsigned short toPort()const;
+
+private:
+	struct sockaddr_in addr_;
+};
